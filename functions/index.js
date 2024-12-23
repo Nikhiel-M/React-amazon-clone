dce91a86c19aee212818e392516b8152218cc249
Nikhiel Maharaj <nikhiel.maharaj05@gmail.com>
2024-12-23 16:50:27 +0200

    rewrote function index.js

--- functions/index.js
+++ functions/index.js
@@ -6,38 +6,22 @@ import express from "express";
 import cors from "cors";
 import Stripe from "stripe";
 
-
 // Initialize Stripe
 const stripe = Stripe(
   "sk_test_51QYP2IL8UBTHhdB9As3M4YktmA9mFafmK1BorpkHuIVCizxzQtpjtdyF5kYIv0rttV6pXcgVT7dCkikiKfX2w4Jd00FSOe3PcY"
 );
 
-// app config
+// API
+
+// App config
 const app = express();
 
-// middlewares
+// Middlewares
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// API routes
-app.get("/", (req, res) => res.status(200).send("Hello world"));
+// API Routes
+app.get("/", (req, res) => res.status(200).send("hello world"));
 
-// Example endpoint (optional)
-app.post("/payments/create", async (req, res) => {
-  const total = req.query.total;
-
-  console.log("Payment Request Received for this amount >>> ", total);
-
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // Subunits of the currency (e.g., cents)
-    currency: "usd",
-  });
-
-  // Send back the client secret for client-side confirmation
-  res.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
-
-// Exports
+// listen command\
 exports.api = functions.https.onRequest(app);
